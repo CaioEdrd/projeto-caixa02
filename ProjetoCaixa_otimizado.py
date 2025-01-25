9a0383c6b358713c3dd99ab6f54db46fad4f6fc0
Gustavo <Gustavo>
2025-01-25 12:07:54 -0300

    Gustavo - Avaliação do projeto - Rascunho da função "conversão" e preparação para a implementação no código principal de funções "Funcoes_ProjetosCaixa.py"

--- ProjetoCaixa_otimizado.py
+++ ProjetoCaixa_otimizado.py
@@ -1,18 +1,24 @@
-import Funcoes_ProjetoCaixa #modularização do arquivo de funções
+import Funcoes_ProjetoCaixa, time #modularização do arquivo de funções
 
 nome_mercado = input("Digite o nome do seu mercado: ")
+time.sleep(1)
 print(f"Bem-Vindo ao controle de caixa {nome_mercado}!")
+time.sleep(1)
 
 print("0 - Help : Exibe todos os comandos disponíveis, como:\n1 - Estoque\n2 - Adicionar\n3 - Vender\n4 - Receita\n5 - Valor Total do estoque\n6 - Ver produto por categoria\n7 - Sair")
+time.sleep(1)
 
 while True:
     print("\nCaso deseje visualizar os comandos novamente, digite 0")
+    time.sleep(1)
     comando = input("Digite o número do comando desejado: ")  #O usuário digita o número do comando desejado
     print()
+    time.sleep(1)
     
     if comando == "0":  #Se o usuário digitar 0, exibe os comandos disponíveis
         print("Comandos disponíveis:\n1 - Estoque\n2 - Adicionar\n3 - Vender\n4 - Receita\n5 - Valor Total do estoque\n6 - Ver produto por categoria\n7 - Sair")
-    
+        time.sleep(1)
+
     elif comando == "1":  #Se o usuário digitar 1, exibe o estoque
         Funcoes_ProjetoCaixa.exibe_estoque()
     
@@ -36,3 +42,4 @@ while True:
     
     else:
         print("Comando inválido!")
+        time.sleep(1)
